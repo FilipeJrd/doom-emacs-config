5373c32eaefefe924220b255bfb8208317a99b1a
Filipe Jordão <filipe.njrd@gmail.com>
2019-10-20 13:47:51 -0300

    Update config

--- config.el
+++ config.el
@@ -1,18 +1,40 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-
-(setq doom-theme 'doom-peacock)
+(mac-auto-operator-composition-mode t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'after-save-hook 'magit-after-save-refresh-status t)
+(setq doom-font (font-spec :family "Fira Code Retina" :size 13))
+(setq doom-theme 'doom-one)
 
 (map! (:map evil-window-map
         "c" nil
         :desc "Delete current window" "d" #'evil-window-delete
-        "S" #'ace-swap-window)
+        "S" #'ace-swap-window
+        "F" #'make-frame)
       (:map workspaces-map
         "TAB" #'+workspace/display))
+
 (map! :leader
+      "0" #'winum-select-window-0-or-10
+      "1" #'winum-select-window-1
+      "2" #'winum-select-window-2
+      "3" #'winum-select-window-3
+      "4" #'winum-select-window-4
+      "5" #'winum-select-window-5
+      "6" #'winum-select-window-6
+      "7" #'winum-select-window-7
+      "8" #'winum-select-window-8
+      "9" #'winum-select-window-9
+      "/" #'+default/search-project
       "`" nil
-      "l" #'evil-switch-to-windows-last-buffer)
+      "l" #'evil-switch-to-windows-last-buffer
+
+      :desc "M-x"                   "SPC"    #'execute-extended-command
+
+      (:prefix ("p" . "project")
+        :desc "Find file in project"         "f" #'projectile-find-file
+        "/" nil))
 
 (map! (:localleader
         (:map (clojure-mode-map clojurescript-mode-map)
@@ -21,3 +43,17 @@
             "l" #'clojure-align)
           (:prefix ("e" . "eval")
             "b" #'cider-load-buffer))))
+
+(use-package! company-quickhelp
+  :hook (company-mode . company-quickhelp-mode))
+
+(add-to-list 'after-make-frame-functions #'winum-mode)
+(after! clojure-mode
+  (define-clojure-indent
+    (fact 1)
+    (facts 1)
+    (flow 1)))
+
+(after! cider-mode
+  (setq cider-overlays-use-font-lock t)
+  (setq cider-repl-pop-to-buffer-on-connect nil))
--- init.el
+++ init.el
@@ -13,10 +13,10 @@
        ;;japanese
 
        :completion
-       company           ; the ultimate code completion backend
+       (company +tng)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +fuzzy +icons)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -37,7 +37,7 @@
         +defaults)       ; default popup rules
        pretty-code       ; replace bits of code with pretty symbols
        ;;tabs              ; an tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -78,7 +78,7 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        eval              ; run code, run (also, repls)
        flycheck          ; tasing you for every semicolon you forget
-       ;;flyspell          ; tasing you for misspelling mispelling
+       flyspell          ; tasing you for misspelling mispelling
        ;;gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +docsets)        ; ...or in Dash docsets locally
@@ -129,7 +129,7 @@
        ;;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +dragndrop       ; drag & drop files/images into org buffers
-        ;+hugo           ; use Emacs for hugo blogging
+                                        ;+hugo           ; use Emacs for hugo blogging
         +ipython         ; ipython/jupyter support for babel
         +pandoc          ; export-with-pandoc support
         +present)        ; using org-mode for presentations
@@ -178,3 +178,47 @@
        ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
        ;; config. Use it as a reference for your own modules.
        (default +bindings +smartparens))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#292D3E" "#ff5370" "#c3e88d" "#ffcb6b" "#82aaff" "#c792ea" "#89DDFF" "#EEFFFF"])
+ '(custom-safe-themes
+   (quote
+    ("2a3ffb7775b2fe3643b179f2046493891b0d1153e57ec74bbe69580b951699ca" "70ed3a0f434c63206a23012d9cdfbe6c6d4bb4685ad64154f37f3c15c10f3b90" "7f74a3b9a1f5e3d31358b48b8f8a1154aab2534fae82c9e918fb389fca776788" "423435c7b0e6c0942f16519fa9e17793da940184a50201a4d932eafe4c94c92d" "0fe9f7a04e7a00ad99ecacc875c8ccb4153204e29d3e57e9669691e6ed8340ce" "5e0b63e0373472b2e1cf1ebcc27058a683166ab544ef701a6e7f2a9f33a23726" "428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" "0d087b2853473609d9efd2e9fbeac088e89f36718c4a4c89c568dd1b628eae41" "001c2ff8afde9c3e707a2eb3e810a0a36fb2b466e96377ac95968e7f8930a7c5" "e7666261f46e2f4f42fd1f9aa1875bdb81d17cc7a121533cad3e0d724f12faf2" "2d392972cbe692ee4ac61dc79907af65051450caf690a8c4d36eb40c1857ba7d" "332e009a832c4d18d92b3a9440671873187ca5b73c2a42fbd4fc67ecf0379b8c" "728eda145ad16686d4bbb8e50d540563573592013b10c3e2defc493f390f7d83" "c8f959fb1ea32ddfc0f50db85fea2e7d86b72bb4d106803018be1c3566fd6c72" "071f5702a5445970105be9456a48423a87b8b9cfa4b1f76d15699b29123fb7d8" "b462d00de785490a0b6861807a360f5c1e05b48a159a99786145de7e3cce3afe" "cdb3e7a8864cede434b168c9a060bf853eeb5b3f9f758310d2a2e23be41a24ae" "a2286409934b11f2f3b7d89b1eaebb965fd63bc1e0be1c159c02e396afb893c8" "f589e634c9ff738341823a5a58fc200341b440611aaa8e0189df85b44533692b" "1728dfd9560bff76a7dc6c3f61e9f4d3e6ef9d017a83a841c117bd9bebe18613" "f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" "f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "2878517f049b28342d7a360fd3f4b227086c4be8f8409f32e0f234d129cee925" "d6f04b6c269500d8a38f3fabadc1caa3c8fdf46e7e63ee15605af75a09d5441e" "fe76f3d5094967034192f6a505085db8db6deb0e135749d9a54dc488d6d3ee2f" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" default)))
+ '(fci-rule-color "#676E95")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1c1f2b" "#c792ea"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f2b" "#c3e88d"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f2b" "#676E95"))
+ '(objed-cursor-color "#ff5370")
+ '(vc-annotate-background "#292D3E")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#c3e88d")
+    (cons 40 "#d7de81")
+    (cons 60 "#ebd476")
+    (cons 80 "#ffcb6b")
+    (cons 100 "#fcb66b")
+    (cons 120 "#f9a16b")
+    (cons 140 "#f78c6c")
+    (cons 160 "#e78e96")
+    (cons 180 "#d690c0")
+    (cons 200 "#c792ea")
+    (cons 220 "#d97dc1")
+    (cons 240 "#ec6898")
+    (cons 260 "#ff5370")
+    (cons 280 "#d95979")
+    (cons 300 "#b36082")
+    (cons 320 "#8d678b")
+    (cons 340 "#676E95")
+    (cons 360 "#676E95")))
+ '(vc-annotate-very-old-color nil)
+ '(winum-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
